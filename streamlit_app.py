824b7d5076c3c8c77a3ca07477835c65fe56e70c
bwelcher18 <brian.welcher@roche.com>
2022-05-26 23:21:35 -0400

    added loc

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #add pick list for fruit
-streamlit.multiselect("Pick Some Fruit:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect("Pick Some Fruit:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 #display the table
 streamlit.dataframe(my_fruit_list)
