ad4d6aec0f8f5077dd9087cde4c1586104bd926a
bwelcher18 <brian.welcher@roche.com>
2022-05-26 23:05:55 -0400

    changed to single import

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import streamlit
+import streamlit pandas
   
 streamlit.title('My Mom\'s New Healthier Dinner')
 streamlit.header('Breakfast Menu')
@@ -9,6 +9,5 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
